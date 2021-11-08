7a6a370720d0133c667c9655deb7fa7115a480c5
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-08 14:36:46 -0800

    Show renders without error.

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Show from './../Show';
+import Show from "./../Show";
 
 const testShow = {
-    //add in approprate test data structure here.
-}
+  //add in approprate test data structure here.
+  name: "Supreme Existence",
+  summary:
+    "Humanity has evolved long without a true test. Now it is time for they they face who have slept below for long.",
+  seasons: [
+    {
+      id: 1,
+      name: "Season 1",
+      episodes: [],
+    },
+    {
+      id: 2,
+      name: "Season 2",
+      episodes: [],
+    },
+  ],
+};
 
-test('renders testShow and no selected Season without errors', ()=>{
+test("renders testShow and no selected Season without errors", () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
 });
 
-test('renders Loading component when prop show is null', () => {
-});
+test("renders Loading component when prop show is null", () => {});
 
-test('renders same number of options seasons are passed in', ()=>{
-});
+test("renders same number of options seasons are passed in", () => {});
 
-test('handleSelect is called when an season is selected', () => {
-});
+test("handleSelect is called when an season is selected", () => {});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
-});
+test("component renders when no seasons are selected and when rerenders with a season passed in", () => {});
 
 //Tasks:
 //1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
